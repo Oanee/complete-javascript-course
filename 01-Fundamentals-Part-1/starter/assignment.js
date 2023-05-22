565b95efa100be8cb420e0660d3cc18f3039affe
Alin Oancea <alin.oancea@keepcalling.com>
2023-05-22 17:44:05 +0300

    first lesson work

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,23 @@
+// 1
+// const country = 'Romania';
+// const continent = 'Europa';
+// let population = 20_000_000;
+//
+// //2
+// const isIsland = false;
+// const language = 'romana';
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
+//
+// console.log(population / 2)
+// console.log(population++)
+//
+// let finlandPopulation = 6_000_000;
+// console.log(population > finlandPopulation)
+
+const markMass = 78;
+const markHeight = 1.69;
+
+console.log(markMass / markHeight ** 2 )
