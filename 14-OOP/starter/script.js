c5f2c36b94ddf38e1ebdca13600662a5a8251146
Alin Oancea <alin.oancea@keepcalling.com>
2023-05-29 16:57:56 +0300

    more learning from work almost done

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,131 @@
 'use strict';
+
+// const Person = function(firstName, birthYear) {
+// 	this.firstName = firstName
+// 	this.birthYear = birthYear
+//
+// 	// this.calcAge = function() {
+// 	// 	console.log(2037 - this.birthYear)
+// 	// }
+// }
+//
+// const jonas = new Person('Jonas', 1991)
+// console.log(jonas)
+//
+// const matilda = new Person('Matilda', 2017)
+// const jack = new Person('Jack', 1975)
+// console.log(matilda)
+// console.log(jack)
+//
+// Person.prototype.calcAge = function() {
+// 	console.log(2037 - this.birthYear)
+// }
+//
+// jonas.calcAge()
+// matilda.calcAge()
+//
+// Person.prototype.species = 'sapiens'
+//
+// console.log(jonas.__proto__.__proto__)
+//
+
+// class PersonCl {
+// 	constructor(fullName, birthYear) {
+// 		this.fullName = fullName
+// 		this.birthYear = birthYear
+// 	}
+//
+// 	calcAge() {
+// 		console.log(2037 - this.birthYear)
+// 	}
+// }
+// //
+// // const jessica = new PersonCl('Jessica', 1996)
+// // console.log(jessica)
+// // jessica.calcAge()
+//
+// const account = {
+// 	owner: 'jonas',
+// 	movements: [200, 530, 120, 300],
+// 	birthYear: 1991,
+//
+// 	get latest() {
+// 		return this.movements.slice(-1).pop()
+// 	},
+//
+// 	set latest(mov) {
+// 		this.movements.push(mov)
+// 	},
+//
+// 	get age() {
+// 		return 2037 - this.birthYear
+// 	}
+// }
+//
+// console.log(account)
+//
+// console.log(account.latest)
+// account.latest = 50
+// console.log(account.movements)
+// console.log(account.age)
+//
+// class StudentCl extends PersonCl {
+// 	constructor(fullName, birthYear, course) {
+// 		super(fullName, birthYear);
+// 		this.course = course
+// 	}
+//
+// 	introduce() {
+// 		console.log(`My name is ${this.fullName} and I study ${this.course}`)
+// 	}
+//
+// 	calcAge() {
+// 		console.log(`i'm ${2037 - this.birthYear} years old, but as a student i feel more like ${2037 - this.birthYear + 10}`)
+// 	}
+// }
+//
+// const martha = new StudentCl('Martha Jones', 2012, 'Computer science')
+// console.log(martha)
+// martha.introduce()
+// martha.calcAge()
+
+class Account {
+	constructor(owner, currency, pin) {
+		this.owner = owner
+		this.currency = currency
+		this.pin = pin
+		this.movements = []
+		this.locale = navigator.language
+
+		console.log(`Thanks for oppening an account ${owner}`)
+	}
+
+	deposit(val) {
+		this.movements.push(val)
+	}
+
+	withdraw(val) {
+		this.deposit(-val)
+	}
+
+	approveLoan(val) {
+		return true
+	}
+
+	requestLoan(val) {
+		if(this.approveLoan(val)) {
+			this.deposit(val)
+			console.log(`Loan Approved`)
+		}
+	}
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111)
+console.log(acc1)
+
+// acc1.movements.push(250)
+// acc1.movements.push(-140)
+
+acc1.deposit(250)
+acc1.withdraw(140)
+acc1.requestLoan(1000)
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -51,37 +51,232 @@ const renderCountry = function(data, className = '') {
         </article>
 			`
 	countriesContainer.insertAdjacentHTML('beforeend', html)
-	countriesContainer.style.opacity = 1
 }
-
-const getCountryAndNeighbor = function (country) {
-	const request = new XMLHttpRequest()
-	request.open('GET', `https://restcountries.com/v2/name/${country}`)
-	request.send()
-
-	request.addEventListener('load', function () {
-		const [data] = JSON.parse(this.responseText)
-		console.log(data)
-
-		renderCountry(data)
-
-		const neighbour = data.borders?.[0]
-
-		if(!neighbour) return
-		const request = new XMLHttpRequest()
-		request.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`)
-		request.send()
-
-		request.addEventListener('load', function() {
-			const data2 = JSON.parse(this.responseText)
-			console.log(data2)
-
-			renderCountry(data2, 'neighbour')
-		})
-	})
-}
-
-getCountryAndNeighbor('portugal')
-getCountryAndNeighbor('romania')
+//
+// const getCountryAndNeighbor = function (country) {
+// 	const request = new XMLHttpRequest()
+// 	request.open('GET', `https://restcountries.com/v2/name/${country}`)
+// 	request.send()
+//
+// 	request.addEventListener('load', function () {
+// 		const [data] = JSON.parse(this.responseText)
+// 		console.log(data)
+//
+// 		renderCountry(data)
+//
+// 		const neighbour = data.borders?.[0]
+//
+// 		if(!neighbour) return
+// 		const request = new XMLHttpRequest()
+// 		request.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`)
+// 		request.send()
+//
+// 		request.addEventListener('load', function() {
+// 			const data2 = JSON.parse(this.responseText)
+// 			console.log(data2)
+//
+// 			renderCountry(data2, 'neighbour')
+// 		})
+// 	})
+// }
+//
+// getCountryAndNeighbor('portugal')
 
 // add new code
+
+// const getCountryData = function (country) {
+// 	fetch(`https://restcountries.com/v2/name/${country}`)
+// 		.then(function(response) {
+// 			console.log(response)
+// 			return response.json()
+// 		})
+// 		.then(function (data) {
+// 			console.log(data)
+// 			renderCountry(data[0])
+// 		})
+// }
+
+const renderError = function(msg) {
+	countriesContainer.insertAdjacentHTML('beforeend', msg)
+}
+
+const getJSON = function(url, errorMSG = `Something went wrong`) {
+	return fetch(url)
+		.then(response=> {
+
+			if(!response.ok) {
+				throw new Error(`${errorMSG} (${response.status})`)
+			}
+
+			return response.json()
+			})
+}
+
+
+// const getCountryData = function (country) {
+// 	fetch(`https://restcountries.com/v2/name/${country}`)
+// 		.then(response=> {
+// 			console.log(response)
+//
+// 			if(!response.ok) {
+// 				throw new Error(`Country not found (${response.status})`)
+// 			}
+//
+// 			return response.json()
+// 		})
+// 		.then(data => {
+// 			renderCountry(data[0])
+// 			const neighbour = data[0].borders[0]
+//
+// 			if(!neighbour) return;
+// 			return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+// 		})
+// 		.then(response => response.json())
+// 		.then(data => renderCountry(data,'neighbour'))
+// 		.catch(err => {
+// 			console.log(`${err} 💥💥💥💥`)
+// 			renderError(`Something went wrong 💥💥 ${err.message}. Try again!`)
+// 		})
+// 		.finally(() => {
+// 			countriesContainer.style.opacity = 1
+// 		})
+// }
+
+// const getCountryData = function (country) {
+// 	getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
+// 		.then(data => {
+// 			renderCountry(data[0])
+// 			const neighbour = data[0].borders[0]
+// 			console.log(neighbour)
+//
+// 			if(!neighbour) {
+// 				throw new Error('No neighbour found!')
+// 			}
+//
+// 			return getJSON(`https://restcountries.com/v2/alpha/${neighbour}`, 'Country not found')
+// 		})
+// 		.then(data => renderCountry(data,'neighbour'))
+// 		.catch(err => {
+// 			console.log(`${err} 💥💥💥💥`)
+// 			renderError(`Something went wrong 💥💥 ${err.message}. Try again!`)
+// 		})
+// 		.finally(() => {
+// 			countriesContainer.style.opacity = 1
+// 		})
+// }
+//
+// btn.addEventListener('click', () => {
+// 	getCountryData('australia')
+// })
+
+
+
+// const whereAmI = function(lat, lng) {
+// 	fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+// 		.then(response => response.json())
+// 		.then(data => console.log(data))
+// }
+
+// whereAmI(52.508, 13.381)
+
+// const lotteryPromise = new Promise(function(resolve, reject) {
+// 	setTimeout(function() {
+// 		if(Math.random() >= 0.5) {
+// 			resolve('You win')
+// 		} else {
+// 			reject(new Error('You lost your money'))
+// 		}
+// 	}, 3000)
+// })
+//
+// lotteryPromise
+// 	.then(res => console.log(res))
+// 	.catch(err => console.log(err))
+//
+// const wait = function(seconds) {
+// 	return new Promise(function(resolve) {
+// 		setTimeout(resolve, seconds * 1000)
+// 	})
+// }
+//
+// wait(2).then(() => {
+// 	console.log('i waited for 2 sec')
+// 	return wait(1)
+// }).then(() => {
+// 	console.log('i waited for 1 sec')
+// })
+//
+//
+// Promise.resolve('abc').then(x => console.log(x))
+// Promise.reject(new Error('Problem')).catch(x => console.error(x))
+
+
+
+// const getPosition = function() {
+// 	return new Promise(function(resolve, reject) {
+// 		// navigator.geolocation.getCurrentPosition(
+// 		// 	position => resolve(position),
+// 		// 	err => reject(err))
+//
+// 		navigator.geolocation.getCurrentPosition(resolve, reject)
+// 	})
+// }
+//
+// getPosition().then(pos => console.log(pos))
+
+// const whereAmI = async function (country) {
+// 	try {
+// 		const res = await fetch(`https://restcountries.com/v2/name/${country}`)
+//
+// 		if(!res.ok) throw new Error('Wrong country')
+//
+// 		const data = await res.json()
+// 		renderCountry(data[0])
+// 		countriesContainer.style.opacity = 1
+//
+// 		return `You are in ${data[0].name}`
+// 	} catch (err) {
+// 		renderError(`Something went wrong ${err.message}`)
+// 		countriesContainer.style.opacity = 1
+//
+// 		throw  err
+// 	}
+// };
+//
+// console.log(1);
+//
+// (async function() {
+// 	try {
+// 		const city = await whereAmI('portugal')
+// 		console.log(2, city)
+//
+// 	} catch (err) {
+//  	console.error(err.message)
+// 	}
+//
+// 	console.log(3)
+// })()
+
+
+const get3Countries = async function(c1, c2, c3) {
+	try {
+		// const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`)
+		// const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`)
+		// const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`)
+		//
+		// console.log([data1.capital, data2.capital, data3.capital])
+
+
+		const data = await Promise.all([
+			getJSON(`https://restcountries.com/v2/name/${c1}`),
+			getJSON(`https://restcountries.com/v2/name/${c2}`),
+			getJSON(`https://restcountries.com/v2/name/${c3}`)])
+
+		console.log(data.map(d => d[0].capital))
+
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+get3Countries('portugal', 'canada', 'tanzania')
